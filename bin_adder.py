d86c3dbd91e5ad154e7f20d16447e872f4fd0b98
hunkiecodie <promise.aeos@gmail.com>
2021-03-30 08:09:25 +0100

    Added student info and caught non-binary numbers

--- bin_adder.py
+++ bin_adder.py
@@ -1,17 +1,10 @@
 # PROGRAMMING A BINARY ADDER - CPE401 LAB WORK
-#
-# 1. Define a half adder which is a XOR to give you a summed output and an AND to give you a carry.
-#   It will have 2 inputs(bit_a, bit_b) and gives two outputs the XOR for sum and the AND for carry.
-#
-# 2. Define a full Adder which is two half adders. It will have 3 inputs (bit_a, bit_b, carry) and
-#    two outputs (sum and carry).
-#
-# 3. Lastly, we call this full adder, starting at the Least Significant Bit (LSB),
-#   with 0 as carry, and work our way to the Most Significant Bit (MSB) where we carry the carry as
-#   input to the next step.
+# NAME: SHITTU PROMISE ADURAGBEMI
+# MATRIC NUMBER: CSC/2016/116
 
 
-def rjust_length(s1, s2, fill='0'):
+# Function to format both inputs to the same number of digits
+def rjust_length(s1, s2, fill="0"):
     l1, l2 = len(s1), len(s2)
     if l1 > l2:
         s2 = s2.rjust(l1, fill)
@@ -20,47 +13,60 @@ def rjust_length(s1, s2, fill='0'):
     return (s1, s2)
 
 
+# Function to get inputs
 def get_inputs():
-    bits_a = input('Input your first binary string:  ')
-    bits_b = input('Input your second binary string: ')
+    bits_a = input("Input your first binary string:  ")
+    bits_b = input("Input your second binary string: ")
     return rjust_length(bits_a, bits_b)
 
 
+# Defining XOR function.
 def xor(bit_a, bit_b):
     A1 = bit_a and (not bit_b)
     A2 = (not bit_a) and bit_b
     return int(A1 or A2)
 
 
+# Defining a half adder which is a XOR to give you a summed output and an AND # to give you a carry. It will have 2 inputs (bit_a, bit_b) and gives two
+# outputs the XOR for sum and the AND for carry.
 def half_adder(bit_a, bit_b):
     return xor(bit_a, bit_b), bit_a and bit_b
 
 
+# Define a full Adder which is two half adders. It will have 3 inputs
+# (bit_a, bit_b, carry) and two outputs (sum and carry).
 def full_adder(bit_a, bit_b, carry=0):
     sum1, carry1 = half_adder(bit_a, bit_b)
     sum2, carry2 = half_adder(sum1, carry)
     return sum2, carry1 or carry2
 
 
+# Lastly, we call this full adder, starting at the Least Significant Bit
+# (LSB), with 0 as carry, and work our way to the Most Significant Bit (MSB)
+# where we carry the carry as input to the next step.
 def binary_string_adder(bits_a, bits_b):
     carry = 0
-    result = ''
+    result = """"""
     for i in range(len(bits_a)-1 , -1, -1):
-        summ, carry = full_adder(int(bits_a[i]), int(bits_b[i]), carry)
-        result += str(summ)
+        sum_, carry = full_adder(int(bits_a[i]), int(bits_b[i]), carry)
+        result += str(sum_)
     result += str(carry)
     return result[::-1]
 
 
+# Main function to run the code.
 def main():
     bits_a, bits_b = get_inputs()
-    print('1st string of bits is : {}, ({})'.format(bits_a, int(bits_a, 2)))
-    print('2nd string of bits is : {}, ({})'.format(bits_b, int(bits_b, 2)))
-    result = binary_string_adder(bits_a, bits_b)
-    print("--"*20)
-    print('Final Adder Result is : {}, ({})'.format(result, int(result, 2)))
-    print("--"*20)
+    try:
+        print("1st string of bits is : {}, ({})".format(bits_a, int(bits_a, 2)))
+        print("2nd string of bits is : {}, ({})".format(bits_b, int(bits_b, 2)))
+        result = binary_string_adder(bits_a, bits_b)
+        print("--" * 20)
+        print("Final Adder Result is : {}, ({})".format(result, int(result, 2)))
+        print("—-" * 20)
+    except ValueError:
+        print("Error in input. Input should be in binary - should contain zero and/or ones.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
