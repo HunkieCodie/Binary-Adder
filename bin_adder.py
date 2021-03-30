39a4fe296630f16bd47725d12b7314bf25ab1cfc
hunkiecodie <promise.aeos@gmail.com>
2021-03-30 07:38:36 +0100

    Added main program file

---
+++ bin_adder.py
@@ -0,0 +1,66 @@
+# PROGRAMMING A BINARY ADDER - CPE401 LAB WORK
+#
+# 1. Define a half adder which is a XOR to give you a summed output and an AND to give you a carry.
+#   It will have 2 inputs(bit_a, bit_b) and gives two outputs the XOR for sum and the AND for carry.
+#
+# 2. Define a full Adder which is two half adders. It will have 3 inputs (bit_a, bit_b, carry) and
+#    two outputs (sum and carry).
+#
+# 3. Lastly, we call this full adder, starting at the Least Significant Bit (LSB),
+#   with 0 as carry, and work our way to the Most Significant Bit (MSB) where we carry the carry as
+#   input to the next step.
+
+
+def rjust_length(s1, s2, fill='0'):
+    l1, l2 = len(s1), len(s2)
+    if l1 > l2:
+        s2 = s2.rjust(l1, fill)
+    elif l2 > l1:
+        s1 = s1.rjust(l2, fill)
+    return (s1, s2)
+
+
+def get_inputs():
+    bits_a = input('Input your first binary string:  ')
+    bits_b = input('Input your second binary string: ')
+    return rjust_length(bits_a, bits_b)
+
+
+def xor(bit_a, bit_b):
+    A1 = bit_a and (not bit_b)
+    A2 = (not bit_a) and bit_b
+    return int(A1 or A2)
+
+
+def half_adder(bit_a, bit_b):
+    return xor(bit_a, bit_b), bit_a and bit_b
+
+
+def full_adder(bit_a, bit_b, carry=0):
+    sum1, carry1 = half_adder(bit_a, bit_b)
+    sum2, carry2 = half_adder(sum1, carry)
+    return sum2, carry1 or carry2
+
+
+def binary_string_adder(bits_a, bits_b):
+    carry = 0
+    result = ''
+    for i in range(len(bits_a)-1 , -1, -1):
+        summ, carry = full_adder(int(bits_a[i]), int(bits_b[i]), carry)
+        result += str(summ)
+    result += str(carry)
+    return result[::-1]
+
+
+def main():
+    bits_a, bits_b = get_inputs()
+    print('1st string of bits is : {}, ({})'.format(bits_a, int(bits_a, 2)))
+    print('2nd string of bits is : {}, ({})'.format(bits_b, int(bits_b, 2)))
+    result = binary_string_adder(bits_a, bits_b)
+    print("--"*20)
+    print('Final Adder Result is : {}, ({})'.format(result, int(result, 2)))
+    print("--"*20)
+
+
+if __name__ == '__main__':
+    main()
